cbb9eb9a4901ed8996932adfe8e46deffd2b1119
Trevisan <jtrevisan2005@gmail.com>
2025-06-05 15:48:10 -0300

    Add files via upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
 
     setCarregando(true);
     try {
-      const response = await fetch(`http://localhost:8080/usuarios/buscar?email=${email}`);
+      const response = await fetch(`https://quarkus-app.onrender.com/usuarios/buscar?email=${email}`);
       if (response.ok) {
         const data = await response.json();
         if (data.nome.toLowerCase() !== nome.toLowerCase()) {
